2f8a7372b759d632b9dcc25994f2446361741fe0
CoderZ <adzohonouapelete@gmail.com>
2023-04-19 15:33:11 +0000

    task update, delete functions

--- controllers/tasks.controllers.js
+++ controllers/tasks.controllers.js
@@ -32,12 +32,35 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("update single task");
+const deleteTask = async (req, res) => {
+  const { id: taskID } = req.params;
+  try {
+    const task = await tasksModels.findOneAndDelete({ _id: taskID });
+    if (!task) return res.status(404).send({ message: "Not found!" });
+    res.status(200).send({ task: null, message: "success!" });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send("delete a task");
+const updateTask = async (req, res) => {
+  const { id: taskID } = req.params;
+  try {
+    const task = await tasksModels.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) return res.status(404).send({ message: "Not found!" });
+    res.status(200).send({ task: task._doc, message: "success!" });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
-module.exports = { getAllTasks, getTask, deleteTask, createTask, updateTask };
+module.exports = {
+  getAllTasks,
+  getTask,
+  deleteTask,
+  createTask,
+  updateTask,
+};
