e03807cd2b7ddbc7b9684ca0524c7cfab3b444d6
CoderZ <adzohonouapelete@gmail.com>
2023-04-19 12:58:47 +0000

    ininitial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const port = 5500
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
+
+//Run app, then load http://localhost:port in a browser to see the output.
